ad3fd4cb7f93875d2efa65e8b46f6446802c5b49
Jonathan Boswell <jonathan@arborbridge.com>
2018-01-24 17:33:11 -0800

    Quiz updates

--- Arrays_wk10/array_checkbox/js/script.js
+++ Arrays_wk10/array_checkbox/js/script.js
@@ -1,18 +1,25 @@
-function onSubmit(){
+$(function(){
     var score = 0;
     var q1 = [];
     var numOfQuestions = 1;
     var pointsPossible = 3;
     var q1ansArr = ['b', 'd', 'e'];
-
-    //loop through question and store the answers into the q1 array
-    for (var i = 0; i < 5; i++) {
-
-        // if statement checks current checks box
-        if (document.forms['quiz']['q1cb' + (i + 1)].checked) {
-            //push users asnwers into q1 array
-            q1.push(document.forms['quiz']['q1cb' + (i + 1)].value);
-        }
+    var answers = {
+        'a': false,
+        'b': true,
+        'c': false,
+        'd': true,
+        'e': true
     }
-alert(q1);
-}
+
+    $('#submit').on('click', function(){
+        for (var i = 0; i < 5; i++) {
+            var guess = $('#q1cb' + (i+1)).is(':checked');
+            var letter = $('#q1cb' + (i+1)).val();
+            if(answers[letter])
+            {
+                $('#q1cb' + (i+1)).css('outline','3px solid green');
+            }
+        }
+    });
+});
